049cde6fa33657c5ee51003612278075ead6a5a7
Fateme Yosefi <86098248+fateme81yosefi@users.noreply.github.com>
2025-02-03 11:46:58 +0330

    add firebase

--- .history/src/App_20250203010104.js
+++ .history/src/App_20250203010104.js
@@ -21,7 +21,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
 function App() {
   return (
     <div className="App">
